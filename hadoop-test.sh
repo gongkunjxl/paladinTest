95c4e734a446c285dff8fd1374a104d5288422a0
gongkunjxl@163.com <gongkunjxl@163.com>
2019-04-02 13:40:07 +0800

    new

--- hadoop-test.sh
+++ hadoop-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-hadoop jar hadoop-mapreduce-client-hobclient-2.7.2-tests.jar TestDFSIO -clean
-hadoop jar hadoop-mapreduce-client-hobclient-2.7.2-tests.jar TestDFSIO -write -nrFiles 10 -size 128
-hadoop jar hadoop-mapreduce-client-hobclient-2.7.2-tests.jar TestDFSIO -read -nrFiles 10 -size 128
+hadoop jar hadoop-mapreduce-client-jobclient-2.7.2-tests.jar TestDFSIO -clean
+hadoop jar hadoop-mapreduce-client-jobclient-2.7.2-tests.jar TestDFSIO -write -nrFiles 10 -size 128
+hadoop jar hadoop-mapreduce-client-jobclient-2.7.2-tests.jar TestDFSIO -read -nrFiles 10 -size 128
