5fca68bbad3a050cccaf3fe26ff2f4017b73cb21
gongkunjxl@163.com <gongkunjxl@163.com>
2019-04-03 15:17:28 +0800

    new

--- spark-test.sh
+++
@@ -1,1 +0,0 @@
-$SPARK_HOME/bin/spark-submit --master Spark://master.gongkun.com:7077 --class org.apache.spark.examples.mllib.JavaSVMWithSGDExample --executor-memory 512m ./target/JavaSVMWithSGDExample-project-1.0.jar
